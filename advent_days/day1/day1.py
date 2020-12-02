d9f796d36509b886aaffc2e916f21f03856d0996
fesieg <Felix.Siegmann@outlok.de>
2020-12-02 22:28:12 +0100

    work on part 2

--- advent_days/day1/day1.py
+++ advent_days/day1/day1.py
@@ -3,8 +3,34 @@ with open("day1input.md") as source:
 expenses = [item.strip() for item in expenses] 
 expenses = [int(item) for item in expenses]
 
-for i, number in enumerate(expenses[:-1]):
-    complementary = 2020 - number
-    if complementary in expenses[i+1:]:
-        print("Solution is: {}*{} = {}".format(number, complementary, number*complementary))
-        break
+def swap(list, a, b):
+    temp = list[b]
+    list[b] = list[a]
+    list[a] = temp
+
+def bubbleSort(list):
+    keepGoing = True
+    while keepGoing:
+        keepGoing = False
+        for n in range(1, len(list)):
+            if list[n] < list [n - 1]:
+                swap(list, n, n-1)
+                keepGoing = True
+
+def findTwo(list, targetNum):
+    for num in range(0, len(list)):
+        needed =  2020 - list[num]
+        print(list[num], needed)
+        if needed in list[num+1:]:
+            return list[num], list[list.index(needed)]
+
+def findThree(list, targetNum):
+    pass
+            
+
+bubbleSort(expenses)
+result = findTwo(expenses, 2020)
+print(result[0] * result[1])
+
+resultPart2 = findThree(expenses, 2020)
+print()
