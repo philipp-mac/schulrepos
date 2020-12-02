03334d58c1239a3947d968515a9d90f660a0f574
fesieg <Felix.Siegmann@outlok.de>
2020-12-02 20:40:12 +0100

    day 1 solved

--- advent_days/day1/day1.py
+++ advent_days/day1/day1.py
@@ -1,3 +1,10 @@
 with open("day1input.md") as source:
     expenses = source.readlines()
 expenses = [item.strip() for item in expenses] 
+expenses = [int(item) for item in expenses]
+
+for i, number in enumerate(expenses[:-1]):
+    complementary = 2020 - number
+    if complementary in expenses[i+1:]:
+        print("Solution is: {}*{} = {}".format(number, complementary, number*complementary))
+        break
