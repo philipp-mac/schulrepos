e6691b571ab158056bcf3eafaad453a216dba692
fesieg <Felix.Siegmann@outlok.de>
2020-12-03 10:40:24 +0100

    day 3

---
+++ advent_days/day3/day3.py
@@ -0,0 +1,26 @@
+from day3input import route
+route = list(filter(bool, route.split("\n")))
+
+def traverseRouteAndCountTrees(x, y, route):
+    treesEncountered = 0
+    currentx = 0
+    currenty = 0
+    for row in range(0, len(route), y):
+        treesEncountered += (route[row][currentx % len(route[0])] == "#")
+        currentx += x
+    return treesEncountered
+
+
+# for item in range(len(route)):
+#     print(route[item], "\n")   
+print("for right 1, down 1: ",traverseRouteAndCountTrees(1, 1, route))
+print("for right 3, down 1: ",traverseRouteAndCountTrees(3, 1, route))
+print("for right 5, down 1: ",traverseRouteAndCountTrees(5, 1, route))
+print("for right 7, down 1: ",traverseRouteAndCountTrees(7, 1, route))
+print("for right 1, down 2: ",traverseRouteAndCountTrees(1, 2, route))
+print(traverseRouteAndCountTrees(1, 1, route) *
+traverseRouteAndCountTrees(3, 1, route) *
+traverseRouteAndCountTrees(5, 1, route) *
+traverseRouteAndCountTrees(7, 1, route) *
+traverseRouteAndCountTrees(1, 2, route))
+
---
+++ advent_days/day3/day3solutions.md
@@ -0,0 +1,8 @@
+part1: 242
+part2:
+for right 1, down 1:  82
+for right 3, down 1:  242
+for right 5, down 1:  71
+for right 7, down 1:  67
+for right 1, down 2:  24
+multiplied: 2265549792
