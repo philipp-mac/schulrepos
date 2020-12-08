029ea363723cf6c139568fba2ff9a9948ea91759
siegmann <siegmann@visus.com>
2020-12-08 18:08:50 +0100

    removed logs

--- advent_days/day8/day8.js
+++ advent_days/day8/day8.js
@@ -10,28 +10,23 @@ function execute(instructions, visited){
     while (visited[index] !== true){
         let command = instructions[index].split(" ")[0]
         let number = Number(instructions[index].split(" ")[1])
-        console.log(command, number);
         switch (command){
             case 'acc':
                 accumulator += number
                 visited[index] = true
                 index++
-                console.log("acc : index = ", index, "accumulator = ", accumulator);
                 break;
             case 'nop':
                 visited[index] = true
                 index++
-                console.log("nop: index = ", index, "accumulator = ", accumulator);
                 break;
             case 'jmp':
                 visited[index] = true
                 index = Math.abs(index + number)
-                console.log("jmp : index = ", index, "accumulator = ", accumulator);
                 break;
         }
         console.log("------------");
     }
-    console.log("BROKE", visited[index] = true);
     return accumulator
 }
 
