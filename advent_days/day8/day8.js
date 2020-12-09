1cacb161c7acc768889725e557723a3038d4ae21
fesieg <Felix.Siegmann@outlok.de>
2020-12-09 11:29:55 +0100

    day8 still broken

--- advent_days/day8/day8.js
+++ advent_days/day8/day8.js
@@ -1,74 +1,94 @@
 const fs = require('fs');
+const { on } = require('process');
 
-let data = fs.readFileSync('day8input.txt', 'utf8');
-const instructions = data.split(/\r?\n/)
-const visited = instructions.map(_ => false)
+let data = fs.readFileSync('day8input.md', 'utf8');
+const baseInstructions = data.split(/\r?\n/).map(i => i.split(" "))
+const visited = baseInstructions.map(_ => false)
 
 function execute(instructions, visited){
-    let steps = 0
     let accumulator = 0
     let index = 0
-    while (index < instructions.length){
-        let command = instructions[index].split(" ")[0]
-        let number = Number(instructions[index].split(" ")[1])
+    while (true){
+        if (index === instructions.length + 1){
+            return ["part2", accumulator]
+        }
+        else if (visited[index] === true){
+            return ["part1", accumulator]
+        }
+        if (instructions[index] === undefined || instructions === undefined || instructions[index][0] === undefined){
+            console.log(" UNDEFINED ,", instructions);
+        }
+        let command = instructions[index][0]
+        let number = Number(instructions[index][1])
         switch (command){
             case 'acc':
                 accumulator += number
                 visited[index] = true
-                steps++
                 index++
                 break;
             case 'nop':
                 visited[index] = true
                 index++
-                steps++
                 break;
             case 'jmp':
                 visited[index] = true
-                steps++
                 index = index + number
                 break;
         }
-    if 
-    if (steps > 10000){
-        return false
-        }
     }
-    console.log("wppp");
-    return accumulator
 }
 
-function autoFixProgram(instructions) {
-    // test each instruction until the program is fixed
-    for (let i = 0; i < instructions.length; i++) {
-        const ins = instructions[i].split(" ");
-        console.log(ins);
-
-        // skip ACCs
-        if (ins[0] === 'acc') {
-            continue;
+function findAllVisited(instructions, visited){
+    let index = 0
+    while (visited[index] != true){
+        let command = instructions[index][0]
+        let number = Number(instructions[index][1])
+        switch (command){
+            case 'acc':
+                visited[index] = true
+                index++
+                break;
+            case 'nop':
+                visited[index] = true
+                index++
+                break;
+            case 'jmp':
+                visited[index] = true
+                index = index + number
+                break;
         }
+    }
+    return instructions.map((hit, index) => visited[index] == true ? hit : null).filter(item => item !== null)
+}
 
-        // backup modified instruction to restore after
-        const insBackup = ins[0];
+function part2(instructions, visited){
+    for (let i = 0; i < instructions.length; i++){
+        let cmd = instructions[i][0]
+            if (cmd === "jmp"){
+                cachedCmd = cmd
+                instructions[i][0] = "nop"
+                console.log(instructions.length);
+                result = execute(instructions, instructions.map(i => false))
+                if (result[0] === "part2"){
+                    return result
+                }
+            instructions[i][0] = cachedCmd
 
-        try {
-            // flip the instruction from jmp -> nop or vice versa
-            ins[0] = (ins[0] === 'jmp') ? 'nop' : 'jmp';
-            console.log(ins[0]);
-
-            // if program exited cleanly, then it is fixed
-            const result = execute(instructions, instructions.map(i => false));
-            if (result != false) {
-                return result;
             }
-
-        } finally {
-            // fix up the instruction before looping or exiting
-            ins[0] = insBackup;
+            else if (cmd === "nop"){
+                cachedCmd = cmd
+                instructions[i][0] = "jmp"
+                result = execute(instructions, instructions.map(i => false))
+                if (result[0] === "part2"){
+                    return result
+                }
+            instructions[i][0] = cachedCmd
         }
     }
 }
 
 // console.log(execute(instructions, visited))
-console.log(autoFixProgram(instructions))
+
+let onlyVisitedInstructions = findAllVisited(baseInstructions, baseInstructions.map(i => false))
+console.log(onlyVisitedInstructions);
+console.log(part2(onlyVisitedInstructions, onlyVisitedInstructions.map(i => false)))
