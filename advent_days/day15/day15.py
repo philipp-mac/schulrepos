a12e67855ad15f8afe2245b04e0fbb48036145fe
fesieg <Felix.Siegmann@outlok.de>
2020-12-17 14:26:11 +0100

    day15 full performance improved

---
+++ advent_days/day15/day15.py
@@ -0,0 +1,45 @@
+from time import perf_counter 
+
+startingnums = (1,0,15,2,10,13)
+
+def addOrUpdateDicts(number, mdict, tdict, i):
+    if number not in mdict.keys():
+        mdict.update({number: [i]})
+        tdict.update({number: 1})
+    else:
+        mdict[number] = [mdict[number][len(mdict[number]) - 1], i]
+        tdict[number] += 1
+
+def play(startingnums, limit):
+    mentions = {}
+    timesMentioned = {}
+    lastNumber = 0
+    timeStart = perf_counter()
+
+    for i in range(len(startingnums)):
+        mentions[startingnums[i]] = [i]
+        timesMentioned[startingnums[i]] = 1
+        lastNumber = startingnums[i]
+
+    print(" ENTERING LOOP \n")
+    for turn in range(len(startingnums), limit):
+        
+        if turn % 1000000 == 0:
+            print("turn ", turn)
+            timeStop = perf_counter()
+            print("time elapsed: approx. ", timeStop - timeStart)
+
+        if timesMentioned[lastNumber] == 1:
+            lastNumber = 0
+            addOrUpdateDicts(lastNumber, mentions, timesMentioned, turn)
+
+        else:
+            previousMentions = mentions[lastNumber][-2:]
+            spoken = previousMentions[1] - previousMentions[0]
+            addOrUpdateDicts(spoken, mentions, timesMentioned, turn)
+            lastNumber = spoken
+
+    return lastNumber
+    
+
+print(play(startingnums, 30000000))
