2e2d6636e0be19b5fe2cb4981da2f9fbc5f47e63
Siegmann, Felix <siegmann@visus.com>
2021-01-04 09:42:27 +0100

    day16 skeleton

---
+++ advent_days/day16/day16.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+with open("day16input.md") as source:
+    data = source.read()
+
+print(data)
+
+rules = defaultdict()
+
+def parseRules(data):
+    pass
+
+def getInvalidAnyField(ticket):
+    print(rules)
+
+def checkAllFields(fields, ticket):
+    pass
+
+
+getInvalidAnyField(1)
