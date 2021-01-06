2cf4ea1812e5e74910b46b55cd6ef48e5554ade2
Siegmann, Felix <siegmann@visus.com>
2021-01-06 13:18:35 +0100

    day16 part1

--- advent_days/day16/day16.py
+++ advent_days/day16/day16.py
@@ -1,19 +1,63 @@
 from collections import defaultdict
-with open("day16input.md") as source:
-    data = source.read()
+import re
 
-print(data)
+# rules = defaultdict()
+validRanges = []
+tickets = []
+invalidFields = []
 
-rules = defaultdict()
+def parseLine(line, mode):
+    ## specific rules parser, may be needed later 
+    # if mode == "rule":
+    #     rule = re.search("^[^:]+", line).group(0)
+    #     ranges = re.search("[^:]+$", line).group(0).strip()
+    #     rules[rule] = ranges
+    #     print(rules[rule]) 
 
-def parseRules(data):
-    pass
+    if mode == "rule":
+        validRanges.append([int(n) for n in re.findall(r'\b\d+\b', line)])
 
-def getInvalidAnyField(ticket):
-    print(rules)
+    if mode == "myTicket" or mode == "otherTickets":
+        numbers = [int(n) for n in line.split(",") if n.strip().isnumeric()]
+        if numbers != []:
+            tickets.append(numbers)
 
-def checkAllFields(fields, ticket):
-    pass
+def readInput():
+    with open("day16input.md") as source:
+        modes = {0: "rule", 1: "myTicket", 2: "otherTickets"}
+        mode = 0
+        for line in source:
+            if len(line) == 1:  
+                mode += 1
+            parseLine(line, modes[mode])
 
 
-getInvalidAnyField(1)
+def formatValidRanges(validRanges):
+    formattedRanges = []
+    for rng in validRanges:
+        formattedRanges.append([[rng[0], rng[1]], [rng[2], rng[3]]])
+    return formattedRanges
+
+def valid(value):
+    for ranges in validRanges:
+        for rng in ranges:
+            if value > rng[0] and value < rng[1]:
+                return True
+    return False
+
+def getInvalidAllRules(tickets):
+    for ticket in tickets:
+        for value in ticket:
+            if not valid(value):
+                invalidFields.append(value)
+
+def calcScanningError(invalidFields):
+    result = 0
+    for item in invalidFields:
+        result += item
+    return result
+
+readInput()
+validRanges = formatValidRanges(validRanges)
+getInvalidAllRules(tickets)
+print(calcScanningError(invalidFields))
