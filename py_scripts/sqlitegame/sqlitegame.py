96609bae1837e67a93f4224f470d58506aae3d98
fesieg <user.email>
2021-08-27 13:06:43 +0200

    working version with player data + highscores

--- py_scripts/sqlitegame/sqlitegame.py
+++ py_scripts/sqlitegame/sqlitegame.py
@@ -4,7 +4,6 @@ import sqlite3, random
 # Verbindung zur DB aufbauen
 conn = sqlite3.connect("game.db")
 
-
 def update_data(player_name: str, guesses: int):
     """
     function that writes a guess to the database
@@ -12,8 +11,8 @@ def update_data(player_name: str, guesses: int):
     :param bool: if the guess was correct
     """
     curs = conn.cursor()
-    curs.execute(f"UPDATE player SET guesses = ? WHERE player_name = ?", (player_name, guesses,))
-    curs.commit()
+    curs.execute(f"update player set guesses = ? WHERE player_name = ?", (guesses, player_name))
+    conn.commit()
 
 def get_guess():
     """
@@ -38,13 +37,23 @@ def end_game(player_name):
     function that outputs data and highscores on game end
     :param player_name: name of current player
     """
+    # output player data
     cursor = conn.cursor()
-    own_data = []
     cursor.execute("SELECT * from player where player_name = ?", (player_name, ))
-    rows = cursor.fetchall()
-    print(rows)
+    row = cursor.fetchall()[0]
+    print(f"Congrats {row[1]}, you took {row[2]} guesses to guess {row[3]}! \n")
+    conn.commit()
 
-    pass
+    # output highscores
+    cursor.execute(f"SELECT player_name, guesses FROM player ORDER BY guesses ASC")
+    rows = cursor.fetchall()
+    print("The top 10 players are: \n")
+    for r in range(10):
+        print(rows[r][0] , f" with {rows[r][1]} guesses! \n")
+
+    # close database connection
+    conn.close()
+
 
 
 def game_loop():
@@ -62,12 +71,12 @@ def game_loop():
             # Call function for user guess input
             curr_guess = get_guess()
 
-            # Write current guess to the sqlite database
-            update_data(player_name, guesses)
-
             # End execution on correct guess
             if curr_guess == winning_num:
                 print("YOU WON!!!")
+
+                # Write guessesto the sqlite database
+                update_data(player_name, guesses)
                 end_game(player_name)
                 break
 
@@ -86,8 +95,9 @@ def prepare_db():
     print("Advanced AI chose a random number between 1 and 100.... \n")
 
     # 3) Make entry in db for this playery
-    conn.execute("INSERT INTO player (player_name, guesses, correctnumber) VALUES (?, ?, ?)", (player_name, 0, winning_num))
+    curs = conn.cursor()
+    curs.execute("INSERT INTO player (player_name, guesses, correctnumber) VALUES (?, ?, ?)", (player_name, 0, winning_num))
+    conn.commit()
 
 prepare_db()
 game_loop()
-conn.close()
